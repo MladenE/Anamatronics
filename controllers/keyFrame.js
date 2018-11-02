1d67958e8652b61cacefe9687a96a309a63362b3
Mladen Edwards <mladen.edwards@monarch.software>
2018-11-02 14:35:40 +0000

    Add logic to build the object for neo4j to UNWIND.

--- controllers/keyFrame.js
+++ controllers/keyFrame.js
@@ -1,6 +1,7 @@
-var express = require('express')
-  , router = express.Router()
-  , validateParams = require('../services/parameters').KeyFrame
+const express        = require('express')
+    , router         = express.Router()
+    , validateParams = require('../services/parameters').KeyFrame
+const uuidv1         = require('uuid/v1'); 
 
 
 // Read
@@ -15,11 +16,63 @@ router.post('/create', function(req, res) {
 
     const validationErrors = validateParams
                                 .init( "Params KeyFrame.Create" )
-                                .KeyFrame.Create( req.user.note )
+                                .KeyFrame.Create( req.user.note )   // Add duration to this.
                                 .GetExceptions();
     
     if ( validationErrors[0].errors.length !== 0 ) { errorCb.send( validationErrors ) };
-    //newName = req.user.newName
+
+
+    // Build object to send to neo4j  <-- This needs to go into a class of it's own to make this less concrete.
+
+    // Get all servoMasters
+    const servoMasters = [];
+    // Create new data object and add id and note for new KeyFrame
+    const data = {
+          keyFrame : { id : uuidv1(), note : req.user.note }
+        , servos : []
+    };
+
+    // Loop through servoMasters: calculate number of transitions needed
+    for (var i; i <= servoMasters.length; i++) 
+    {
+        // Create the servo
+        const servo = {
+              id        : uuidv1()
+            , masterId  : servoMasters[i].id
+            , note      : ""
+            , positions : {}
+        }
+
+        // Calc the number of transitions ( including Head position ) needed for this servo.
+        const transitionCount = Math.ceil( req.user.duration / servoMasters[i].speed );
+
+        // The starting position for each transition. Half way between pwm0 and pwm180
+        const defaultPosition = ( servoMasters[i].pwm180Degrees + servoMasters[i].pwm0Degrees ) / 2; 
+
+        // Create positions to hold the head and transitions. Add the head position.
+        const positions = {
+              head        : { id : uuidv1(), position : defaultPosition, note : '' }
+            , transitions : []
+        }
+
+        // Add the transitions to postions
+        for (const p; p <= transitionCount -1; p++)
+        {
+            const transition = {
+                  id       : uuidv1()
+                , position : defaultPosition
+                , note     : ''
+            }
+
+            // Add transitions to position.transitions
+            positions.transitions.push(transition);
+        }
+
+        servo.positions.push( positions );
+
+        data.servos.push( servo );
+    }
+
 })
 
 // Update
