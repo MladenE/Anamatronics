959ea0ac4724648839fa96c87d3f6ac73784ed7f
Mladen Edwards <mladen.edwards@monarch.software>
2018-11-29 13:43:54 +0000

    Promise intergration. Not finished.

--- controllers/keyFrame.js
+++ controllers/keyFrame.js
@@ -2,6 +2,8 @@ const express        = require('express')
     , router         = express.Router()
     , validateParams = require('../services/parameters').KeyFrame
 const uuidv1         = require('uuid/v1'); 
+const keyFrame       = require('../models/keyFrame');
+const servoMaster    = require('../models/servoMaster');
 
 
 // Read
@@ -21,59 +23,12 @@ router.post('/create', function(req, res) {
     
     if ( validationErrors[0].errors.length !== 0 ) { errorCb.send( validationErrors ) };
 
-
-    // Build object to send to neo4j  <-- This needs to go into a class of it's own to make this less concrete.
-
     // Get all servoMasters
+    servoMasters.getAll( keyFrame.create );
     const servoMasters = [];
 
-    // Create new data object and add id and note for new KeyFrame
-    // See NOTES > "Example KeyFrame object for Neo4j"
-    const data = {
-          keyFrame : { id : uuidv1(), note : req.user.note }
-        , servos : []
-    };
-
-    // Loop through servoMasters: calculate number of transitions needed
-    for (var i; i <= servoMasters.length; i++) 
-    {
-        // Create the servo
-        const servo = {
-              id        : uuidv1()
-            , masterId  : servoMasters[i].id
-            , note      : ""
-            , positions : {}
-        }
-
-        // Calc the number of transitions ( including Head position ) needed for this servo.
-        const transitionCount = Math.ceil( req.user.duration / servoMasters[i].speed );
-
-        // The starting position for each transition. Half way between pwm0 and pwm180
-        const defaultPosition = ( servoMasters[i].pwm180Degrees + servoMasters[i].pwm0Degrees ) / 2; 
-
-        // Create positions to hold the head and transitions. Add the head position.
-        const positions = {
-              head        : { id : uuidv1(), position : defaultPosition, note : '' }
-            , transitions : []
-        }
-
-        // Add the transitions to postions
-        for (const p; p <= transitionCount -1; p++)
-        {
-            const transition = {
-                  id       : uuidv1()
-                , position : defaultPosition
-                , note     : ''
-            }
-
-            // Add transitions to position.transitions
-            positions.transitions.push( transition );
-        }
-
-        servo.positions.push( positions );
-
-        data.servos.push( servo );
-    }
+    // Build an object to send to neo4j 
+    const data = keyFrame.create( servoMasters );
 
     // Call the neo4j cypher and send it data
 
--- models/keyFrame.js
+++ models/keyFrame.js
@@ -1,6 +1,4 @@
-
-const db = require('../services/neo4j')
-
+const db = require('../services/neo4j');
 
 exports.getAllForAction = function (actionId, cb) {
 
@@ -19,19 +17,78 @@ exports.getForId = function (keyFrameId, cb) {
     
 }
 
-exports.create = (keyFrameNote, db, query, params, cb) => {
+exports.create = (servoMasters) => {
 
-    return db(query, params);
 
-    // or this 
-    
-    const createServo = db.Cyphers.Servos.Create;
+    // Called via cb from GetServos
+
+    // Calls private function to format the javascript object
+    var unwindData = _formatKeyframeObjectForNeo4j( servoMasters );
+
+    // Calls neo4j to create the keyframes
     db.RunCypher(
-        createServo.cypher,
-        createServo.params()
-    )
+        db.Cyphers.Servos.Create.cypher
+      , unwindData
+      , errorCb     
+      , ( result ) => { res.send( result ); } // What to do on success              
+  );
+
 }
 
 exports.updateNote = function (keyFrameId, keyFrameNote, cb) {
     
+}
+
+// Only used by Neo4j because it's the only DB that uses UNWIND
+function _formatKeyframeObjectForNeo4j( servoMasters )
+{
+    // Create new data object and add id and note for new KeyFrame
+    // See NOTES > "Example KeyFrame object for Neo4j"
+    const unwindData = {
+        keyFrame : { id : uuidv1(), note : req.user.note }
+      , servos : []
+    };
+
+    // Loop through servoMasters: calculate number of transitions needed
+    for (var i; i <= servoMasters.length; i++) 
+    {
+        // Create the servo
+        const servo = {
+                id        : uuidv1()
+            , masterId  : servoMasters[i].id
+            , note      : ''
+            , positions : {}
+        }
+
+        // Calc the number of transitions ( including Head position ) needed for this servo.
+        const transitionCount = Math.ceil( req.user.duration / servoMasters[i].speed );
+
+        // The starting position for each transition. Half way between pwm0 and pwm180
+        const defaultPosition = ( servoMasters[i].pwm180Degrees + servoMasters[i].pwm0Degrees ) / 2; 
+
+        // Create positions to hold the head and transitions. Add the head position.
+        const positions = {
+                head        : { id : uuidv1(), position : defaultPosition, note : '' }
+            , transitions : []
+        }
+
+        // Add the transitions to postions
+        for (const p; p <= transitionCount -1; p++)
+        {
+            const transition = {
+                    id       : uuidv1()
+                , position : defaultPosition
+                , note     : ''
+            }
+
+            // Add transitions to position.transitions
+            positions.transitions.push( transition );
+        }
+
+        servo.positions.push( positions );
+
+        data.servos.push( servo );
+    }
+    
+    return unwindData;    
 }
